3d0ebb7fcd27c27109ad1f2c58f9837c81696266
Jorge Luis Roa <jlroa82@misena.edu.co>
2023-04-18 11:58:15 -0500

    feat(project):define files for pays

---
+++ models/Pay.js
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+import bcryp from "bcryptjs";
+
+const PaySquema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    tpcontrato: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    valor: {
+      type: Number,
+        required: true,
+    },
+    status: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    //propiedad para crear la fecha de creacion y de actualizacion actuamaticamnete
+    timestamps: true,
+  }
+);
+
+
+export default model("Pay", PaySquema);
